62d7ac62e7e7eb55ab8f6fe9e36a74578364cf50
tonybnya <nya.tony2010@gmail.com>
2025-09-23 08:00:14 +0100

    feat: add String Mirror challenge

---
+++ tests/test_023_is_mirror.py
@@ -0,0 +1,23 @@
+"""
+Script Name : test_023_is_mirror.py
+Description : Tests for String Mirror challenge
+Author      : @tonybnya
+"""
+import pytest
+from challenge_023_is_mirror import is_mirror
+
+
+@pytest.mark.parametrize(
+    "str1, str2, expected",
+    [
+        ("helloworld", "helloworld", False),
+        ("Hello World", "dlroW olleH", True),
+        ("RaceCar", "raCecaR", True),
+        ("RaceCar", "RaceCar", False),
+        ("Mirror", "rorrim", False),
+        ("Hello World", "dlroW-olleH", True),
+        ("Hello World", "!dlroW !olleH", True)
+    ]
+)
+def test_is_mirror(str1: str, str2: str, expected: bool) -> None:
+    assert is_mirror(str1, str2) == expected
