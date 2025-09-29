4209f0f929cea7e0fee09841b1ce6d29fc81e695
tonybnya <nya.tony2010@gmail.com>
2025-09-29 21:13:38 +0100

    feat: add Longest Word challenge

---
+++ tests/test_029_get_longest_word.py
@@ -0,0 +1,19 @@
+"""
+Script Name : test_029_get_longest_word.py
+Description : Test suite for Longest Word challenge
+Author      : @tonybnya
+"""
+import pytest
+from challenge_029_get_longest_word import get_longest_word
+
+
+@pytest.mark.parametrize(
+    "sentence, expected",
+    [
+        ("coding is fun", "coding"),
+        ("Coding challenges are fun and educational.", "educational"),
+        ("This sentence has multiple long words.", "sentence")
+    ]
+)
+def test_get_longest_word(sentence: str, expected: str) -> None:
+    assert get_longest_word(sentence) == expected
