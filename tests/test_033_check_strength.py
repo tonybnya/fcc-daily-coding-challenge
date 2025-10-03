3ca5450931cd52d47a9554ed97d10943bb87001d
tonybnya <nya.tony2010@gmail.com>
2025-10-03 09:15:23 +0100

    feat: add Password Strength challenge

---
+++ tests/test_033_check_strength.py
@@ -0,0 +1,27 @@
+"""
+Script Name : test_033_check_strength.py
+Description : Test suite for Password Check challenge
+Author      : @tonybnya
+"""
+import pytest
+from challenge_033_check_strength import check_strength
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        ("123456", "weak"),
+        ("pass!!!", "weak"),
+        ("Qwerty", "weak"),
+        ("PASSWORD", "weak"),
+        ("PASSWORD!", "medium"),
+        ("PassWord%^!", "medium"),
+        ("qwerty12345", "medium"),
+        ("PASSWORD!", "medium"),
+        ("PASSWORD!", "medium"),
+        ("S3cur3P@ssw0rd", "strong"),
+        ("C0d3&Fun!", "strong")
+    ]
+)
+def test_check_strength(password: str, expected: str) -> None:
+    assert check_strength(password) == expected
