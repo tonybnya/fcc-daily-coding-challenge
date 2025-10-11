750674efb8bcbb82d6c067168d1295df9eeda598
tonybnya <nya.tony2010@gmail.com>
2025-10-11 09:27:52 +0100

    feat: implement Hex to Decimal

---
+++ tests/test_041_hex_to_decimal.py
@@ -0,0 +1,30 @@
+"""
+Script Name : test_041_hex_to_decimal.py
+Description : Test suite for Hex to Decimal
+Author      : @tonybnya
+"""
+
+import pytest
+from challenge_041_hex_to_decimal import hex_to_decimal
+
+
+@pytest.mark.parametrize(
+    "hex, decimal",
+    [
+        ("0", 0),
+        ("1", 1),
+        ("9", 9),
+        ("A", 10),
+        ("F", 15),
+        ("10", 16),
+        ("9F", 159),
+        ("A0", 160),
+        ("FF", 255),
+        ("100", 256),
+        ("15", 21),
+        ("2E", 46),
+        ("A3F", 2623),
+    ],
+)
+def test_hex_to_decimal(hex: str, decimal: int) -> None:
+    assert hex_to_decimal(hex) == decimal
