2de0d64c7c7a07f464c6252c646e67e3d87735c0
tonybnya <nya.tony2010@gmail.com>
2025-10-12 17:18:20 +0100

    feat: implement Battle of Words coding challenge

---
+++ tests/test_042_battle.py
@@ -0,0 +1,24 @@
+"""
+Script Name : test_042_battle.py
+Description : Test suite for Battle of Words
+Author      : @tonybnya
+"""
+
+import pytest
+from challenge_042_battle import battle
+
+
+@pytest.mark.parametrize(
+    "our_team, opponent, expected",
+    [
+        ("hello world", "hello word", "We win"),
+        ("Hello world", "hello world", "We win"),
+        ("lorem ipsum", "kitty ipsum", "We lose"),
+        ("hello world", "world hello", "Draw"),
+        ("git checkout", "git switch", "We win"),
+        ("Cheeseburger with fries", "Cheeseburger with Fries", "We lose"),
+        ("We must never surrender", "Our team must win", "Draw"),
+    ],
+)
+def test_battle(our_team: str, opponent: str, expected: str) -> None:
+    assert battle(our_team, opponent) == expected
