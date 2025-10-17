96f9b8bfaccb470e82cb14c7c8bc3a3accc9e1e2
tonybnya <nya.tony2010@gmail.com>
2025-10-17 09:29:20 +0100

    feat: implement Credit Card Masker coding challenge

---
+++ tests/test_047_credit_card_masker.py
@@ -0,0 +1,21 @@
+"""
+Script Name : test_047_credit_card_masker.py
+Description : Test suite for Credit Card Masker coding challenge
+Author      : @tonybnya
+"""
+
+import pytest
+from challenge_047_credit_card_masker import mask
+
+
+@pytest.mark.parametrize(
+    "card, expected",
+    [
+        ("4012-8888-8888-1881", "****-****-****-1881"),
+        ("5105 1051 0510 5100", "**** **** **** 5100"),
+        ("6011 1111 1111 1117", "**** **** **** 1117"),
+        ("2223-0000-4845-0010", "****-****-****-0010"),
+    ],
+)
+def test_credit_card_masker(card: str, expected: str) -> None:
+    assert mask(card) == expected
