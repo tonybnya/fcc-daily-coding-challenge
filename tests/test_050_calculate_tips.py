0b8bd4c2c0cfc7aab02039e11db211298681a3cd
tonybnya <nya.tony2010@gmail.com>
2025-10-20 11:11:37 +0100

    feat: implement Tip Calculator coding challenge

---
+++ tests/test_050_calculate_tips.py
@@ -0,0 +1,20 @@
+"""
+Script Name : test_050_calculate_tips.py
+Description : Test suite fot Tip Calculator coding challenge
+Author      : @tonybnya
+"""
+
+import pytest
+from challenge_050_calculate_tips import calculate_tips
+
+
+@pytest.mark.parametrize(
+    "meal_price, custom_tip, expected",
+    [
+        ("$10.00", "25%", ["$1.50", "$2.00", "$2.50"]),
+        ("$89.67", "26%", ["$13.45", "$17.93", "$23.31"]),
+        ("$19.85", "9%", ["$2.98", "$3.97", "$1.79"]),
+    ],
+)
+def test_calculate_tips(meal_price: str, custom_tip: str, expected: list[str]) -> None:
+    assert calculate_tips(meal_price, custom_tip) == expected
