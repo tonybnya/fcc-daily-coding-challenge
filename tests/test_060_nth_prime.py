47f5f10bd7c590a51736e19f088b16f3dfee73e4
tonybnya <nya.tony2010@gmail.com>
2025-10-31 21:12:10 +0100

    feat: implement Nth Prime coding challenge

---
+++ tests/test_060_nth_prime.py
@@ -0,0 +1,15 @@
+"""
+Script Name : test_060_nth_prime.py
+Description : Test suite for Nth Prime
+Author      : @tonybnya
+"""
+
+import pytest
+from challenge_060_nth_prime import nth_prime
+
+
+@pytest.mark.parametrize(
+    "n, expected", [(5, 11), (10, 29), (16, 53), (99, 523), (1000, 7919)]
+)
+def test_nth_prime(n: int, expected: int) -> None:
+    assert nth_prime(n) == expected
